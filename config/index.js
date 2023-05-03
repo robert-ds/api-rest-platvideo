2cb40a7e5da8453919bca3bdf7ae95f17ac9533d
Robert Vásquez <vasquezrober09@gmail.com>
2023-05-03 15:50:07 -0400

    finished lib mongo and persitence data

--- config/index.js
+++ config/index.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 
 const config = {
   dev: process.env.NODE_ENV !== 'production',
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  cors: process.env.CORS,
+  dbUSER: process.env.DB_USER,
+  dbPASSWORD: process.env.DB_PASSWORD,
+  dbHOST: process.env.DB_HOST,
+  dbNAME: process.env.DB_NAME
 }
 
 module.exports = { config };
