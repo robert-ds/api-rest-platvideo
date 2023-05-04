dacedf455bcfc90fa44882800a827283f2597f5d
Robert Vásquez <vasquezrober09@gmail.com>
2023-05-04 12:23:46 -0400

    added cache to endpoints

--- routes/movies.js
+++ routes/movies.js
@@ -3,6 +3,8 @@ const MoviesService = require('../services/movies');
 const { movieIdSchema, createMovieSchema, updateMovieSchema  } = require('../utils/schemas/movies');
 
 const validationHandler = require('../utils/middleware/validationHandler');
+const cacheResponse = require('../utils/cacheResponse');
+const { FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS } = require('../utils/time');
 
 const moviesApi = (app) => {
   const router = express.Router();
@@ -11,7 +13,9 @@ const moviesApi = (app) => {
   const movieService = new MoviesService();
 
   router.get("/", async function(req, res, next) {
+    cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
     const { tags } = req.query;
+
     try {
       const movies = await Promise.resolve(movieService.getMovies({ tags }));
       res.status(200).json({
@@ -24,6 +28,8 @@ const moviesApi = (app) => {
   });
 
   router.get("/:movieId", validationHandler( {movieId: movieIdSchema}, 'params' ),  async function(req, res, next) {
+    cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
+
     const { movieId } = req.params;
     try {
       const movie = await movieService.getMovie({ movieId });
